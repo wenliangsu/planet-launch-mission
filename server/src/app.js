57ba99c1bca6e2618141d64d5662d0264e6b7949
Wen <wenliangsu65018@gmail.com>
2023-06-17 13:08:46 +0800

    Feat: build the react app on the server side and set the deploy script

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
+
 const planetsRouter = require('./routes/planets/planets.router');
 
 const app = express();
@@ -10,6 +12,12 @@ app.use(cors({
 
 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'public', 'index.html'));
+});
 
 module.exports = app;
