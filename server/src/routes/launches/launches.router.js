095b9dc5f2ffe4d7300046f95953f4d579c7d40b
Wen <wenliangsu65018@gmail.com>
2023-06-19 22:38:06 +0800

    Chore: install the jest package

--- server/src/routes/launches/launches.router.js
+++ server/src/routes/launches/launches.router.js
@@ -5,6 +5,7 @@ const launchesRouter = express.Router();
 
 launchesRouter.get('/', httpGetAllLaunches);
 launchesRouter.post('/', httpAddNewLaunch);
+// also can use PUT method, it can be decided by how the data you want to operate 
 launchesRouter.delete('/:id', httpAbortLaunch);
 
 module.exports = launchesRouter;
