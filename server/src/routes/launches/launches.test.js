4ea3120c8730c096175a91a0ba1a1085415a64a2
Wen <wenliangsu65018@gmail.com>
2023-06-20 10:05:39 +0800

    add the launches test file

---
+++ server/src/routes/launches/launches.test.js
@@ -0,0 +1,21 @@
+describe('Test GET /Launches', () => {
+  test('It should respond with 200 success', () => {
+    const respond = 200;
+    expect(respond).toBe(200);
+  });
+});
+
+describe('Test POST /Launches', () => {
+  test('It should respond with 200 success', () => {
+
+  });
+
+  test('It should catch missing required properties', () => {
+
+  });
+
+  test('It should catch invalid dates', () => {
+
+  });
+
+});
